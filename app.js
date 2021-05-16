c20630cadcd9e110551912fd1011c94cef591ad2
Austin Elliott <74209030+a3lliott@users.noreply.github.com>
2021-05-16 17:20:22 -0500

    Added game functionality to show results of rounds

--- app.js
+++ app.js
@@ -1,15 +1,79 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+function getComputerChoice() {
+  const choices = ['r', 'p', 's'];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+function convertToWord(letter) {
+  if (letter === 'r') {
+    return "Rock";
+  } else if (letter === 'p') {
+    return "Paper";
+  } else if (letter === 's') {
+    return "Scissors";
+  } else {
+    return "Invalid character";
+  }
+}
+
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  const smallUserWord = "user".fontsize(3).sub();
+  const smallCompWord = "comp".fontsize(3).sub();
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}. You Win!`;
+  document.getElementById(userChoice).classList.add("green-glow");
+  setTimeout(function () { document.getElementById(userChoice).classList.remove("green-glow"); }, 1000);
+}
+
+function lose(userChoice, computerChoice) {
+  computerScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  const smallUserWord = "user".fontsize(3).sub();
+  const smallCompWord = "comp".fontsize(3).sub();
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord}. You Lose...`;
+}
+
+function draw(userChoice, computerChoice) {
+  const smallUserWord = "user".fontsize(3).sub();
+  const smallCompWord = "comp".fontsize(3).sub();
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals ${convertToWord(computerChoice)}${smallCompWord}. It's a draw.`;
+}
+
 function game(userChoice) {
-  console.log("poop" + userChoice);
+  const computerChoice = getComputerChoice();
+  switch(userChoice + computerChoice) {
+    case "rs":
+    case "pr":
+    case "sp":
+      win(userChoice, computerChoice);
+      break;
+    case "sr":
+    case "rp":
+    case "ps":
+      lose(userChoice, computerChoice);
+      break;
+    case "rr":
+    case "pp":
+    case "ss":
+      draw(userChoice, computerChoice);
+      break;
+    default:
+      console.log("Invalid entry");
+      break;
+  }
 }
 
 function main() {
